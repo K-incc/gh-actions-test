8460884ad45c58afc2efa10fef84236438806725
kinc <kinc.developer@gmail.com>
2020-04-23 22:06:01 +0300

    Updates

---
+++ precache-manifest.a90a2d193c6badede74bc317de61b990.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31cc17a9a0ee62e123050af214c8a4a9",
+    "url": "/gh-actions-test/index.html"
+  },
+  {
+    "revision": "cf553bad704f3d348831",
+    "url": "/gh-actions-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c0a19aeb8009d7560006",
+    "url": "/gh-actions-test/static/js/2.0de2bc9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-actions-test/static/js/2.0de2bc9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf553bad704f3d348831",
+    "url": "/gh-actions-test/static/js/main.1cfeea92.chunk.js"
+  },
+  {
+    "revision": "721cff9439c4eeb37514",
+    "url": "/gh-actions-test/static/js/runtime-main.d82411ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-actions-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
